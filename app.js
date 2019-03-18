b3796d6f235407a8d9c4695891655659814eb044
asuluntas <aysunsulun@gmail.com>
2019-03-18 14:49:06 -0700

    Places X and O's on the board.

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 console.log("tictactoe");
 console.log(document);
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   // Handler when the DOM is fully loaded
+// });
+
+var turn = true;
 document.getElementById("table").addEventListener("click", function(e){
   //console.log(e.target);
   var x = document.createTextNode("X");
-  e.target.appendChild(x);
+  var o = document.createTextNode("O");
+
+  if (turn) {
+    e.target.appendChild(x);
+    turn = !turn;
+  } else {
+    e.target.appendChild(o);
+    turn = !turn;
+  }
 
 });
 
 
-var turn = true;
-
-var togglePiece = function() {
-
-  var x = document.createTextNode("X");
-  var o = document.createTextNode("O");
-
-  document.getElementById("table").addEventListener("click", function(e){
-  if (true) {
-    e.target.appendchild(x);
-    turn = !turn;
-  }
-  e.target.appendchild(o);
-  turn = !turn;
-  });
-
-}
 
