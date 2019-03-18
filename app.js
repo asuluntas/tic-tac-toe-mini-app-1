2f7effe4eead2318ab2bdb81c0886b00690bf7d7
asuluntas <aysunsulun@gmail.com>
2019-03-18 16:42:46 -0700

    Added classes in html

--- app.js
+++ app.js
@@ -7,24 +7,26 @@ console.log(document);
 
 var turn = true;
 document.getElementById("table").addEventListener("click", function(e){
-  //console.log(e.target);
+
   var x = document.createTextNode("X");
   var o = document.createTextNode("O");
 
-  if (e.target.classList.contains("cell")) {
+  if (e.target.classList.contains("x") || e.target.classList.contains("o")) {
     console.log("Please select another square!");
   } else {
     if (turn) {
       e.target.appendChild(x);
-      e.target.classList.add("cell");
+      e.target.classList.add("x");
       turn = !turn;
     } else {
       e.target.appendChild(o);
-      e.target.classList.add("cell");
+      e.target.classList.add("o");
       turn = !turn;
     }
   }
 });
 
-
+function checkForWin(sign) {
+  if ()
+}
 
